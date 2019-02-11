514ae7d8ee0c0e9710f937880ba8ae89d52ca0b6
RawKitMan <cjvaughn@rocketmail.com>
2019-02-11 10:02:01 -0700

    Completed troubleshooting of bugs. Will not post if form is not complete.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,9 +11,10 @@ module.exports = function (app) {
     //This gets the user data from survey.html and puts it into the friends array. 
     app.post("/api/friends", function (req, res) {
         let newUser = req.body;  
+        console.log(newUser);
         
         //We only want fully completed forms to be pushed in. If anything is missing, nothing gets through.
-        if (newUser.name !== "" && newUser.imageURL !== "" && newUser.responses.length === 10) {
+        if (newUser.name !== "" && newUser.imageURL !== "" && typeof newUser.responses !== "undefined" && newUser.responses.length === 10) {
             friendData.push(newUser);
         };
     });
